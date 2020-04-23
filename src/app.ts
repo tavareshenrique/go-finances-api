f6b2cd8b9e02c59742325ea0127def79d72ceb98
Henrique Tavares <ihenrits@gmail.com>
2020-04-22 21:11:53 -0300

    feat: adicionando cors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
+import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppError';
 import uploadConfig from './config/upload';
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
